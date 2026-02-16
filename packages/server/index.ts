18376146bdf0b5075f6c1d12996549fcbcc9265e
Titya Yem <thitya.yem.photo@gmail.com>
2026-02-16 13:04:24 -0500

    run both apps together

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,14 +1,18 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 const app = express();
 dotenv.config();
 
 app.get("/", (req, res) => {
-    res.send("Lamdouy2");
-})
+  res.send("Lamdouy");
+});
+
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Lamdouy2" });
+});
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-})
+  console.log(`Server is running on http://localhost:${port}`);
+});
