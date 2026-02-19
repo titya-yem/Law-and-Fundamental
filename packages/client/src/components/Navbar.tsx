45a03ffc1b0b890751031c6a9ebdf75c20bd3d95
Titya Yem <thitya.yem.photo@gmail.com>
2026-02-19 05:01:16 -0500

    Finished: Navbar 90%

--- packages/client/src/components/Navbar.tsx
+++ packages/client/src/components/Navbar.tsx
@@ -1,34 +1,39 @@
 import logo from '@/assets/Logo.jpg';
 import { NavbarAuth, NavbarLists } from '@/constants/NavbarLists';
+import { Flex } from '@radix-ui/themes';
 import { NavLink } from 'react-router';
 
 // Check if link is active show semi-bold font
 const getNavClass = ({ isActive }: { isActive: boolean }) =>
-  isActive ? 'font-semibold' : 'text-gray-700';
+  isActive ? 'font-semibold' : 'text-gray-700 hover:underline delay-200';
 
 const Navbar = () => {
   return (
-    <header className="flex justify-between items-center p-4">
-      <img
-        src={logo}
-        alt="Law and Fdamental Local Mediation Office Logo"
-        className="w-13.75"
-      />
+    <header>
+      <Flex justify="between" align="center" p="4">
+        <img
+          src={logo}
+          alt="Law and Fdamental Local Mediation Office Logo"
+          className="w-13.75 lg:w-16.25"
+        />
 
-      <nav className="flex gap-x-3">
-        {NavbarLists.map(({ label, link }) => (
-          <NavLink key={link} to={link} className={getNavClass}>
-            {label}
-          </NavLink>
-        ))}
+        <nav>
+          <Flex gapX="4" className="*:text-sm lg:*:text-base">
+            {NavbarLists.map(({ label, link }) => (
+              <NavLink key={link} to={link} className={getNavClass}>
+                {label}
+              </NavLink>
+            ))}
 
-        {/* for Auth */}
-        {NavbarAuth.map(({ label, link }) => (
-          <NavLink key={link} to={link} className={getNavClass}>
-            {label}
-          </NavLink>
-        ))}
-      </nav>
+            {/* for Auth */}
+            {NavbarAuth.map(({ label, link }) => (
+              <NavLink key={link} to={link} className={getNavClass}>
+                {label}
+              </NavLink>
+            ))}
+          </Flex>
+        </nav>
+      </Flex>
     </header>
   );
 };
--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,29 +1,38 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'; // ✅ import correctly
+import { Theme } from '@radix-ui/themes';
 import './index.css';
-import AuthLayout from './layouts/AuthLayout';
+
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
-import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
+import MainLayout from './layouts/MainLayout';
+import AuthLayout from './layouts/AuthLayout';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
+    <QueryClientProvider client={queryClient}>
+      <Theme>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MainLayout />}>
+              <Route index element={<Home />} />
 
-          <Route element={<AuthLayout />}>
-            <Route path="register" element={<Register />} />
-            <Route path="login" element={<Login />} />
-          </Route>
+              <Route element={<AuthLayout />}>
+                <Route path="register" element={<Register />} />
+                <Route path="login" element={<Login />} />
+              </Route>
 
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Theme>
+    </QueryClientProvider>
   </StrictMode>
 );
