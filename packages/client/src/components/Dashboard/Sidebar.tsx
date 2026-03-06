1dda5f3a5d62dbc7485725a07e199abb5ecfa13e
Titya Yem <thitya.yem.photo@gmail.com>
2026-03-06 14:28:08 -0500

    UI: create sidebar

---
+++ packages/client/src/components/Dashboard/Sidebar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Sidebar = () => {
+  return (
+    <div className="w-64 h-full bg-gray-100 p-4">
+      <ul>
+        <li>Dashboard</li>
+        <li>Settings</li>
+        <li>Profile</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
--- packages/client/src/layouts/DashboardLayout.tsx
+++ packages/client/src/layouts/DashboardLayout.tsx
@@ -1,12 +1,18 @@
+import Sidebar from '@/components/Dashboard/Sidebar';
 import Footer from '@/components/Footer';
 import { Outlet } from 'react-router';
 
 const DashboardLayout = () => {
   return (
-    <>
-      <Outlet />
+    <div className="flex flex-col min-h-screen">
+      <div className="flex flex-1">
+        <Sidebar />
+        <main className="flex-1 p-4">
+          <Outlet />
+        </main>
+      </div>
       <Footer />
-    </>
+    </div>
   );
 };
 
