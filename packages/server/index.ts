66a2e52b8a6431f5c049809d1f74bc23b86b4690
Titya Yem <thitya.yem.photo@gmail.com>
2026-03-23 15:42:49 -0400

    Fixed: index.ts backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -29,7 +29,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Backend is running ✅" });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
