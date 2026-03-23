aa38d487a36af58cdd86f420a301e3fab525a2e1
Titya Yem <thitya.yem.photo@gmail.com>
2026-03-22 21:17:23 -0400

    debug: vercel.json

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,35 +1,35 @@
 import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import cookieParser from "cookie-parser"
-import helmet from "helmet";
-import morgan from "morgan"
-
-import userRouter from "./src/routes/user.route"
-import caseRouter from "./src/routes/case.route"
-import BackUpRouter from "./src/routes/backup.route"
 import ServerlessHttp from "serverless-http";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import morgan from "morgan";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+import userRouter from "./src/routes/user.route";
+import caseRouter from "./src/routes/case.route";
+import BackUpRouter from "./src/routes/backup.route";
 
 const app = express();
-dotenv.config(); // anable to use env file
 
 // middleware
-app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:5173",
-  credentials: true,
-}))
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
-app.use(helmet()); 
+app.use(helmet());
 app.use(morgan("dev"));
 app.use(cookieParser());
 
+// routes
 app.use("/api/auth", userRouter);
 app.use("/api/case", caseRouter);
 app.use("/api/backup", BackUpRouter);
 
+// root health check
 app.get("/", (req, res) => {
-  res.json({ message: "E-Commerce Backend is running ✅" });
+  res.json({ message: "Backend is running ✅" });
 });
 
-
+// export as serverless function
 export default ServerlessHttp(app);
